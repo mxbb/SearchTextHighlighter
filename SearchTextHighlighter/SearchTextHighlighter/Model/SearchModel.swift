487ef0a13219220206afab83a879a5b334678e07
IvuninMK <maxtpu@gmail.com>
2019-09-28 17:42:57 +0600

    sample project 1 iteration

---
+++ SearchTextHighlighter/SearchTextHighlighter/Model/SearchModel.swift
@@ -0,0 +1,13 @@
+//
+//  SearchModelSample.swift
+//  SearchTextHighlighter
+//
+//  Created by Maxim on 28/09/2019.
+//  Copyright © 2019 Maxim Ivunin. All rights reserved.
+//
+
+import Foundation
+
+class SearchModel: SearchModelProtocol {
+    
+}
---
+++ SearchTextHighlighter/SearchTextHighlighter/View/SearchCell.swift
@@ -0,0 +1,24 @@
+//
+//  SearchCell.swift
+//  SearchTextHighlighter
+//
+//  Created by Maxim on 28/09/2019.
+//  Copyright © 2019 Maxim Ivunin. All rights reserved.
+//
+
+import UIKit
+
+class SearchCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
